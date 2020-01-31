6e8694a4490398cc2756379e396c59f09653179f
Samuel Oliveira <br.samuel.oliveira@gmail.com>
2020-01-31 16:47:14 -0400

    Implementado Regressao Linear Simples

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # regressaoLinear
-regressão linear é uma equação para se estimar a condicional (valor esperado) de uma variável y, dados os valores de algumas outras variáveis x.
+Regressão Linear é uma equação para se estimar a condicional (valor esperado) de uma variável y, dados os valores de algumas outras variáveis x.
---
+++ regressaoLinear.py
@@ -0,0 +1,24 @@
+def regressao_linear(x, y, X):
+   
+    somaX = 0.0
+    somaY = 0.0
+    somaXY = 0.0
+    somaXQuad = 0.0
+    for i in range(len(x)):
+
+        somaX += float(x[i])
+        somaY += float(y[i])
+        somaXY += float(x[i]) * float(y[i])
+        somaXQuad += float(x[i]) ** 2
+
+    #Determinando Alpha
+    alpha = len(x) * somaXY - somaX * somaY
+    alpha /= len(x) * somaXQuad - somaX ** 2
+
+    mediaX = somaX / len(x)
+    mediaY = somaY / len(y)
+    #Determinando Beta
+    beta = mediaY - alpha * mediaX
+
+    Y = alpha * X + beta
+    return Y
